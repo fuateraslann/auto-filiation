fb45e77f19a8eb5ec0fd87e4d2dbf12254fb61e6
tarikkeskin <tarik.keskin007@gmail.com>
2020-12-21 23:48:32 +0300

    User location permission implementation..

--- app/src/main/java/com/autofiliation/autofiliation/LoginActivity.java
+++ app/src/main/java/com/autofiliation/autofiliation/LoginActivity.java
@@ -21,6 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private EditText emailText, passwordText;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/autofiliation/autofiliation/MainActivity.java
+++ app/src/main/java/com/autofiliation/autofiliation/MainActivity.java
@@ -2,12 +2,17 @@ package com.autofiliation.autofiliation;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -19,6 +24,9 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
+    private final int PERMISSION_ID = 20;
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
@@ -44,8 +52,56 @@ public class MainActivity extends AppCompatActivity {
 
         firebaseAuth= FirebaseAuth.getInstance();
 
-        setAlarm();
+        if(!checkPermissions()){
+            requestPermissions();
+        }
+        else{
+            setAlarm();
+        }
+        if(!isLocationEnabled()){
+            Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+
     }
+
+
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) this.getSystemService(this.LOCATION_SERVICE);
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    // method to check for permissions
+    private boolean checkPermissions() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    // method to request for permissions
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[] {
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_BACKGROUND_LOCATION},
+                PERMISSION_ID);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 20) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //if permission is granted, set alarm
+                setAlarm();
+                //myLocationManager.requestLocationUpdate();
+            }
+        }
+    }
+
 
     private void setAlarm(){
         //getting the alarm manager
